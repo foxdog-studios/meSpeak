aa659a2707a7258c86b326e3847b971c418193b5
Lloyd Henning <lloyd.w.henning@gmail.com>
2015-06-08 15:41:52 +0100

    Wrapped up for Meteor

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'fds:mespeak',
+  version: '0.0.1',
+  summary: 'Text-To-Speech on the Web - works with web audio ',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('mespeak.js', ['client']);
+  api.add_files('mespeak_config.json', ['client'], {isAsset: true});
+  api.add_files('voices/en/en.json', ['client'], {isAsset: true});
+  api.export('meSpeak');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('fds:mespeak');
+  api.addFiles('mespeak-tests.js');
+});
